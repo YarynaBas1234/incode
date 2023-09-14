b5bd507c27be3b57e28e6dad1bb2068f5ba77326
YarynaBas1234 <jaryna.bas@gmail.com>
2023-09-14 21:21:05 +0300

    styled empty state

--- src/components/EmptyState/EmptyState.tsx
+++ src/components/EmptyState/EmptyState.tsx
@@ -1,5 +1,21 @@
+import { Body } from 'components';
+import { styled, theme } from 'styles';
+
+const EmptyStateContainer = styled.div`
+	width: 100%;
+	min-height: 400px;
+	padding: 50px 0;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+`;
+
 export const EmptyState = () => {
-  return (
-    <div>EmptyState</div>
-  )
-}
+	return (
+		<EmptyStateContainer>
+			<Body fontSize='40px' leading='60px' color={theme.colors.white}>
+				No matches with your criteria.
+			</Body>
+		</EmptyStateContainer>
+	);
+};
